fe4f03fad99c2bc9614ae1b608ef34a22dcfb762
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-02-16 19:15:37 +0100

    Use 24 bits for colors

--- main.zig
+++ main.zig
@@ -3,11 +3,11 @@ const std = @import("std");
 const Canvas = struct {
     width: u32,
     height: u32,
-    data: []u8, // A single slice representing all the canvas
+    data: []u24, // A single slice representing all the canvas
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator, width: u32, height: u32) !Canvas {
-        const data = try allocator.alloc(u8, width * height);
+        const data = try allocator.alloc(u24, width * height);
         return Canvas{
             .width = width,
             .height = height,
@@ -20,18 +20,18 @@ const Canvas = struct {
         self.allocator.free(self.data);
     }
 
-    pub fn get(self: *const Canvas, x: u32, y: u32) u8 {
+    pub fn get(self: *const Canvas, x: u32, y: u32) u24 {
         return self.data[y * self.width + x];
     }
 
     // Set a pixel's color, with the origin to the top left of the canvas
-    fn set(self: *Canvas, x: u32, y: u32, color: u8) void {
+    fn set(self: *Canvas, x: u32, y: u32, color: u24) void {
         self.data[y * self.width + x] = color;
     }
 
     // Put a pixel on the canvas, using a coordinate system with the origin
     // at the center of the canvas
-    pub fn put_pixel(self: *Canvas, x: u32, y: u32, color: u8) void {
+    pub fn put_pixel(self: *Canvas, x: u32, y: u32, color: u32) void {
         const converted_x = x + self.width / 2;
         const converted_y = y - self.height / 2;
 
