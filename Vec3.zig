8561c244a13dd199b4973f5c671601e53a0f3bb7
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-02-17 17:55:07 +0100

    Add Vec3 struct

---
+++ Vec3.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+const Vec3 = struct {
+    x: f64,
+    y: f64,
+    z: f64,
+
+    pub fn dot(self: Vec3, other: Vec3) f64 {
+        return self.x * other.x + self.y * other.y + self.z * other.z;
+    }
+
+    pub fn cross(self: Vec3, other: Vec3) Vec3 {
+        return Vec3{ .x = self.y * other.z - self.z * other.y, .y = self.z * other.x - self.x * other.z, .z = self.x * other.y - self.y * other.x };
+    }
+};
