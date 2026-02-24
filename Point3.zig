b96d89ef0b57cc8af7f0c2e5ae090458645c40d9
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-02-24 18:15:12 +0100

    Add add and subtract methods to Point3

--- Point3.zig
+++ Point3.zig
@@ -1,7 +1,25 @@
 const std = @import("std");
 
+const Vec3 = @import("Vec3.zig");
+
 pub const Point3 = struct {
     x: f64,
     y: f64,
     z: f64,
+
+    pub fn add(self: Point3, other: Point3) Point3 {
+        Point3{
+            .x = self.x + other.x,
+            .y = self.y + other.y,
+            .z = self.z + other.z,
+        };
+    }
+
+    pub fn subtract(self: Point3, other: Point3) Vec3 {
+        Vec3{
+            .x = self.x - other.x,
+            .y = self.y - other.y,
+            .z = self.z - other.z,
+        };
+    }
 };
